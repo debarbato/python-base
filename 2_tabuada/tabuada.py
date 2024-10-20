6ca2e0c60ff5450dbb64488d9a00c05acdd1e14c
David Barbato <daviddev1306@gmail.com>
2024-10-20 19:22:09 -0300

    melhorar output

--- 2_tabuada/tabuada.py
+++ 2_tabuada/tabuada.py
@@ -1,16 +1,34 @@
 #!/usr/bin/env python3
 """Imprime a tabuada do 1 ao 10 """
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "David"
 
-
-base = [1,2,3,4,5,6,7,8,9,10]
-print(base)
-print(type(base))
+#print(base)
+#print(type(base))
 numeros = list(range(1,11))
 
 # Iterable (percorrer)
-for numero in numeros:
-    print("Tabuada do:", numero)
-    for outro_numero in numeros:
-        print(numero, "X", outro_numero, "=",  outro_numero * numero)
+# for numero in numeros:
+    # print("Tabuada do:", numero)
+    # for outro_numero in numeros:
+        # print(numero, "X", outro_numero, "=",  outro_numero * numero)
+
+for n1 in numeros:
+    print('{:-^21}'.format(f"Tabuada do {n1}"))
+    print()
+    for n2 in numeros:
+        resultado = n1 * n2
+        print("{:^18}".format(f"{n1} x {n2} = {resultado}"))
+    print(30*'#')
+
+
+
+
+
+
+
+
+
+
+
+
